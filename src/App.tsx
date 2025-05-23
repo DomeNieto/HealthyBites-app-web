f16a8a791db7f05767b440fd2e52677b2c0c0fc2
DomenicaNieto <usr3857@salesianos-lacuesta.net>
2025-05-23 03:12:45 +0100

    feat(): Add useTheme App.

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,20 @@ import { Provider } from "react-redux";
 
 import { store } from "./store/store";
 import Router from "./pages/router/Router";
-import { Box, ThemeProvider } from "@mui/material";
-import theme from "./themes/CustomTheme";
+import { Box, ThemeProvider, useTheme } from "@mui/material";
 
+/**
+ * Main application component.
+ *
+ * Wraps the app with Redux Provider and Material-UI ThemeProvider.
+ * Provides the Redux store and MUI theme to all child components.
+ *
+ * Layout uses a flex container that fills the viewport height,
+ * with the Router component taking flexible space inside.
+ */
 function App() {
+  const theme = useTheme();
+
   return (
     <Provider store={store}>
       <ThemeProvider theme={theme}>
